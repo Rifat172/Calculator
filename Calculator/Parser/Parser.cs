4ce6f78e0b23f52178e1e2b778c1f0b5da2fbd13
Rifat172 <45rifat7227@gmail.com>
2019-07-17 12:05:40 +0500

    Added: ParserTest.Parse_DifficultGoodExpression_GoodArray() Parser.Parse(string) for DEC

---
+++ Calculator/Parser/Parser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SourceExpression
+{
+    public class Parser
+    {
+        string[] ArrayExpression = new string[] { "2", "*", "2", "+", "2" };
+        public string[] Parse(string exp)
+        {
+            ArrayExpression = foo(exp);
+            return ArrayExpression;
+        }
+        private string[] foo(string exp)
+        {
+            List<string> arr = new List<string>();
+
+            Regex regex = new Regex(@"\+|\-|\/|\*|\(|\)|[0-9]+");
+
+            var matches = regex.Matches(exp);
+
+            foreach (Match match in matches)
+            {
+                arr.Add(match.Value);
+            }
+            return arr.ToArray();
+        }
+    }
+}
---
+++ Calculator/SourceExpressionTest/ParserTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using SourceExpression;
+
+namespace SourceExpressionTest
+{
+    [TestFixture]
+    class ParserTest
+    {
+        [Test]
+        public void Parse_SimpleGoodExpression_GoodArray()
+        {
+            Parser parser = new Parser();
+            string[] ArrayExpression = parser.Parse("2*2+2");
+            Assert.AreEqual(new string[] { "2", "*", "2", "+", "2" }, ArrayExpression);
+        }
+        [Test]
+        public void Parse_SimpleGoodExpression_BadArray()
+        {
+            Parser parser = new Parser();
+            string[] ArrayExpression = parser.Parse("2+2*2");
+            Assert.AreNotEqual(new string[] { "2", "*", "2", "+", "2" }, ArrayExpression);
+        }
+        
+        [Test]
+        public void Parse_DifficultGoodExpression_GoodArray()
+        {
+            Parser parser = new Parser();
+            string[] ArrayExpression = parser.Parse("(22+2)*2");
+            Assert.AreEqual(new string[] { "(", "22", "+", "2", ")", "*", "2" }, ArrayExpression);
+        }
+    }
+}
