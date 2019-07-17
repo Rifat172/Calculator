ae52237919bab9d3f94eb74a2ff582e87ae460ab
Rifat172 <45rifat7227@gmail.com>
2019-07-17 20:24:21 +0500

    метод для конвертирования Инфиксной нотации к Постфиксной нотации

---
+++ Calculator/RPNAlgorithm/Convert.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPNAlgorithm
+{
+    public static class Convert
+    {
+        public static string[] ToRPN(string[] exp)
+        {
+            double value = 0;
+            List<string> OutPut = new List<string>();
+            Dictionary<string, short> PriorityTable = new Dictionary<string, short>();
+            Stack<string> stack = new Stack<string>();
+            InitPriorityTable(PriorityTable);
+            stack.Push("#");
+            for (int i = 0; i < exp.Length; i++)
+            {
+                if(exp[i]=="(")
+                {
+                    stack.Push(exp[i]);
+                }
+                else if(exp[i]==")")
+                {
+                    while(stack.Peek()!="(")
+                    {
+                        OutPut.Add(stack.Pop());
+                    }
+                    stack.Pop();
+                }
+                else if (double.TryParse(exp[i], out value))
+                {
+                    OutPut.Add(exp[i]);
+                }
+                else
+                {
+                    if(LeftOperationPriorityMoreThanRight(exp[i],stack.Peek(),PriorityTable))
+                    {
+                        stack.Push(exp[i]);
+                    }
+                    else
+                    {
+                        string temp;
+                        if(LeftOperationPriorityMoreThanRight(temp=stack.Pop(),stack.Peek(),PriorityTable))
+                        {
+                            OutPut.Add(temp);
+                            stack.Push(exp[i]);
+                        }
+                    }
+                }
+            }
+            while (stack.Count != 1)
+                OutPut.Add(stack.Pop());
+            return OutPut.ToArray();
+        }
+
+        private static bool LeftOperationPriorityMoreThanRight(string left, string right, Dictionary<string,short> priorityTable)
+        {
+            if(Math.Max(priorityTable[left],priorityTable[right])==priorityTable[left])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static void InitPriorityTable(Dictionary<string, short> priorityTable)
+        {
+            priorityTable.Add("#", 0);
+            priorityTable.Add("(", 0);
+            priorityTable.Add(")", 0);
+            priorityTable.Add("+", 1);
+            priorityTable.Add("-", 1);
+            priorityTable.Add("*", 2);
+            priorityTable.Add("/", 2);
+        }
+    }
+}
---
+++ Calculator/RPNAlgorithmTest/Converttest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using RPNAlgorithm;
+
+namespace RPNAlgorithmTest
+{
+    [TestFixture]
+    class ConvertTest
+    {
+        [TestCase(new string[] { "3", "+", "2" }, new string[] { "3", "2", "+"})]
+        [TestCase(new string[] { "3", "-", "2" }, new string[] { "3", "2", "-"})]
+        [TestCase(new string[] { "(", "3", "-","2",")","*","5","+","10,1" }, new string[] { "3", "2", "-","5","*","10,1","+"})]
+        public void StaticToRPN_CorrectlyInput_CorrectlyOutPut(string[] InPut, string[] ExcpectedOutPut)
+        {
+            string[] RPNExpression = Convert.ToRPN(InPut);
+            Assert.AreEqual(ExcpectedOutPut, RPNExpression);
+        }
+    }
+}
